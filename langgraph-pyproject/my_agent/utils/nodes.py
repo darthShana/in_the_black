994ffb8e5e9c7d3a0c74c3dea072e30115184377
DHarshana Ratnayake <dharshana.ratnayake@turners.co.nz>
2024-08-19 18:21:19 +1200

    fix deployment

--- langgraph-pyproject/my_agent/utils/nodes.py
+++ langgraph-pyproject/my_agent/utils/nodes.py
@@ -1,20 +1,9 @@
 from functools import lru_cache
 from langchain_anthropic import ChatAnthropic
-from my_agent.utils.tools import tools
 from langgraph.prebuilt import ToolNode
 from langchain_core.runnables import RunnableLambda
 from langchain_core.messages import ToolMessage
 
-
-@lru_cache(maxsize=4)
-def _get_model(model_name: str):
-    if model_name == "anthropic":
-        model =  ChatAnthropic(temperature=0, model_name="claude-3-sonnet-20240229")
-    else:
-        raise ValueError(f"Unsupported model type: {model_name}")
-
-    model = model.bind_tools(tools)
-    return model
 
 # Define the function that determines whether to continue or not
 def should_continue(state):
@@ -29,19 +18,6 @@ def should_continue(state):
 
 
 system_prompt = """Be a helpful assistant"""
-
-# Define the function that calls the model
-def call_model(state, config):
-    messages = state["messages"]
-    messages = [{"role": "system", "content": system_prompt}] + messages
-    model_name = config.get('configurable', {}).get("model_name", "anthropic")
-    model = _get_model(model_name)
-    response = model.invoke(messages)
-    # We return a list, because this will get added to the existing list
-    return {"messages": [response]}
-
-# Define the function to execute tools
-tool_node = ToolNode(tools)
 
 def handle_tool_error(state) -> dict:
     error = state.get("error")
